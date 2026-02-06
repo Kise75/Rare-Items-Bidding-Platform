1fa0a8eab3e4ce5ba4dab9a90d0f1309db1f4509
Tan Phat <maitanphat200475@gmail.com>
2026-02-06 22:53:35 +0700

    Hoàn thành bài tập Spring Boot: khởi tạo project với Spring Initializr + thêm RestController /welcome trả về 'Welcome to my web application'

---
+++ src/main/java/com/maitanphat/rareitems/RareItemsBiddingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.maitanphat.rareitems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RareItemsBiddingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RareItemsBiddingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maitanphat/rareitems/RareItemsBiddingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maitanphat.rareitems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RareItemsBiddingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
